0869a2b31462e0caadf82917795c90ac11d299d8
Rahma Yahia <rahmahamam521@gmail.com>
2026-09-10 08:08:34 +0300

    Add role-based authorization with admin/user roles and protect sensitive user routes

---
+++ middleware/authorizeRoles.js
@@ -0,0 +1,15 @@
+function authorizeRoles(...allowedRoles) {
+  return function (req, res, next) {
+    if (!req.user) {
+      return res.status(401).json({ success: false, message: 'Authentication required' });
+    }
+
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ success: false, message: 'Access denied' });
+    }
+
+    next();
+  };
+}
+
+module.exports = authorizeRoles;
